9c1a316d09082c52592aa32926aef4fadc106463
danieleppler <46427281+danieleppler@users.noreply.github.com>
2020-11-14 17:11:03 +0200

    Login and Register views not completed

---
+++ app/src/main/java/com/example/dimot_bekalot2/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.dimot_bekalot2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
