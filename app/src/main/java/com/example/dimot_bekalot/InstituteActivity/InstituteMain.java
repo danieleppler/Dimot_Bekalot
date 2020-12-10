b9c6e0424e226939a5384a9dc65c4eb7012f52ce
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-12-11 00:07:23 +0200

    f

--- app/src/main/java/com/example/dimot_bekalot/InstituteActivity/InstituteMain.java
+++ app/src/main/java/com/example/dimot_bekalot/InstituteActivity/InstituteMain.java
@@ -6,18 +6,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dimot_bekalot.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class InstituteMain extends AppCompatActivity implements View.OnClickListener {
     private Button add, watching, institute_data;
     private TextView institute_name;
     private static final String INSTITUTE = "institutes";
     private String institute_id = "";
+    private String nameInstitute = "";
 
     private FirebaseDatabase dataBase;
     private DatabaseReference dbRef;
@@ -35,8 +40,19 @@ public class InstituteMain extends AppCompatActivity implements View.OnClickList
         Intent institute_details = getIntent();
         institute_id = institute_details.getExtras().getString("instituteID");
         dataBase = FirebaseDatabase.getInstance();
-        dbRef = dataBase.getReference(INSTITUTE).child(institute_id).child("institute_name");
-        String nameInstitute = dbRef.toString();
+        dbRef = dataBase.getReference(INSTITUTE);
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                nameInstitute = snapshot.child(institute_id).child("institute_name").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         /* name of institute */
         institute_name = (TextView)findViewById(R.id.name_institute);
